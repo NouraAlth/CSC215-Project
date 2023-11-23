ef780b078eb13813d8c582aede8a431c75ebaf85
Atom2033 <140852498+Atom2033@users.noreply.github.com>
2023-11-23 22:25:10 +0300

    function prototype and taxies struct
    implementation is not done yet

---
+++ Project-Files/Taxies.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+struct Taxies
+{
+    int id;
+char driver[20];
+char category[20];
+char plate[7];
+char color[20];
+float rate;
+ float minCharge;
+ char state;
+ //  pointer next,pointer list
+};
+void setTripCar (char* category, float* rate);
+
+
+int main(){
+
+    return 0;
+}
